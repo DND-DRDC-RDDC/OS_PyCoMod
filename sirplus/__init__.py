2e1cf5f6b6369cb4aff82e566fac18f89bffb183
stephen-okazawa-gc <86741975+stephen-okazawa-gc@users.noreply.github.com>
2021-08-19 16:55:16 -0400

    Delete sirplus directory

--- sirplus/__init__.py
+++
@@ -1,4 +0,0 @@
-from sirplus.elements import pool, flow, parameter, sample, equation, step, impulse
-from sirplus.model import model
-from sirplus.manager import run_manager
-from sirplus.plotter import plotter
